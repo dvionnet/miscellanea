84a5f0f8f9c9a89acd3f7066ec899414665e7237
David Vionnet <david.vionnet@vmailbox.org>
2014-12-10 18:29:08 +0100

    Couldn't find a working example of it with Google, had to do it myself.

---
+++ ycombinator.exs
@@ -0,0 +1,25 @@
+# The Y Combinator is defined as
+# Y = λf.(λx.f(xx))(λx.f(xx))
+#
+# it's possible to implement simple recursion with it:
+#
+# Yg = (λf.(λx.f(xx))(λx.f(xx)))g
+#    = (λx.g(xx))(λx.g(xx))
+#    = (λy.g(yy))(λx.g(xx))
+#    = g(λx.g(xx))(λx.g(xx))
+#    = g(Yg)
+#    = g(g(Yg))
+
+# the Y combinator
+y = fn f -> (&(&1.(&1))).(&(f.(fn a -> (&1.(&1)).(a) end))) end
+
+# Let's take 2 examples, the factorial and the fibonacci functions
+fac = &(fn 0 -> 0; 1->1; n -> n*&1.(n-1) end)
+fib = &(fn 0 -> 0; 1 -> 1; n -> &1.(n-1) + &1.(n-2) end)
+
+IO.puts "fac(100) = #{(y.(fac)).(100)}"
+IO.puts "fib(20) = #{(y.(fib)).(20)}"
+IO.puts """
+  The Chief programmer said I was allowed to use
+  that kind of trick, so yeah, why not? :)
+  """
